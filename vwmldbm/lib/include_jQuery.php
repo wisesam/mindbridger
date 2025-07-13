2d520b3d4034be1c2da637eaf86cbd5276da9f39
Sam Han <lalasjhan@gmail.com>
2025-07-13 17:16:23 +0900

    path

--- vwmldbm/lib/include_jQuery.php
+++ vwmldbm/lib/include_jQuery.php
@@ -7,9 +7,8 @@
 /*
   Description : jquery links
 */
-require_once("../config.php");
+
 echo "<link rel='stylesheet' href='{$VWMLDBM['VWMLDBM_WWW_RT']}/lib/jquery/ui/1.12.1/jquery-ui.css'>";
 echo "<script src='{$VWMLDBM['VWMLDBM_WWW_RT']}/lib/jquery/jquery-3.2.1.min.js'></script>";
 echo "<script src='{$VWMLDBM['VWMLDBM_WWW_RT']}/lib/jquery/ui/1.12.1/jquery-ui.min.js'></script>";
-
 ?>
