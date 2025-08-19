3cc34f9b293301f6d68e2cfbfbe308b2c7a5bf91
Sam Han <lalasjhan@gmail.com>
2025-08-20 08:03:18 +0900

    m

--- vwmldbm/lib/system.php
+++ vwmldbm/lib/system.php
@@ -10,6 +10,3 @@ function isAdmin() { // check if the user is admin
 	if(isAuth() && isset($_SESSION['wlibrary_admin']) && ($_SESSION['wlibrary_admin']!='A' || $_SESSION['wlibrary_admin']!='SA')) return true;
 	return false;
 }
-
-
-
